5c11234f7981f6c3e1fc3ddf48665ec6a008451c
Chris Wesseling <chris.wesseling@cwi.nl>
2011-08-16 19:50:48 +0200

    should-dsl matcher for Dingus calls

---
+++ should_dingus.py
@@ -0,0 +1,52 @@
+from should_dsl import matcher
+
+
+class Call(object):
+    
+    name = 'call'
+
+    def __call__(self, called_dingus):
+        self._called_dingus = called_dingus
+        self._times = None
+        self._args = []
+        self._kwargs = {}
+        return self
+
+    def __getattr__(self, rterm):
+        if rterm == 'once':
+            self._times = 1
+        return self
+
+    def with_params(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+        return self
+
+    def match(self, function):
+        self._called_dingus.reset()
+        function()
+        self._calls = self._called_dingus.calls('()',
+                *self._args, **self._kwargs)
+        if self._times:
+            return len(self._calls) == self._times
+        else:
+            return len(self._calls) > 0
+
+    def message_for_failed_should(self):
+        msg = repr(self._called_dingus)
+        if not self._calls:
+            msg += " has not been called"
+        else:
+            msg += (" was called %d times" % len(self._calls))
+
+        if self._args or self._kwargs:
+            msg += " with params %r %r" % (self._args, self._kwargs)
+
+        if self._times:
+            msg += ", expected %d" % self._times
+        return msg
+
+    def message_for_failed_should_not(self):
+        return "%s was unexpectedly called" % repr(self._called_dingus)
+
+matcher(Call)
---
+++ tests.py
@@ -0,0 +1,76 @@
+import should_dingus
+from dingus import Dingus
+from should_dsl import should, should_not, ShouldNotSatisfied
+
+
+class TestCall:
+    def setup(self):
+        self.dinges = Dingus('callable')
+
+    def test_simple_call(self):
+        (lambda: self.dinges()) |should| call(self.dinges)
+
+    def test_nothing_called(self):
+        (lambda: None) |should_not| call(self.dinges)
+
+    def test_unexpected_call(self):
+        def wrapper():
+            (lambda: self.dinges()) |should_not| call(self.dinges)
+        msg = '<Dingus callable> was unexpectedly called'
+        wrapper |should| throw(ShouldNotSatisfied, msg)
+
+    def test_expected_call_not_happened(self):
+        def wrapper():
+            (lambda: None) |should| call(self.dinges)
+        msg = '<Dingus callable> has not been called'
+        wrapper |should| throw(ShouldNotSatisfied, msg)
+
+    def test_call_once(self):
+        (lambda: self.dinges()) |should| call(self.dinges).once
+
+    def test_too_many_calls(self):
+        def wrapper():
+            def actions():
+                self.dinges()
+                self.dinges()
+            actions |should| call(self.dinges).once
+        msg = '<Dingus callable> was called 2 times, expected 1'
+        wrapper |should| throw(ShouldNotSatisfied, msg)
+
+    def test_call_with_positional_param(self):
+        def wrapper():
+            self.dinges(1)
+        wrapper |should| call(self.dinges).with_params(1)
+
+    def test_call_with_wrong_positional_param(self):
+        def wrapper():
+            def actions():
+                self.dinges(2)
+            actions |should| call(self.dinges).with_params(1)
+        msg = '<Dingus callable> has not been called with params (1,) {}'
+        wrapper |should| throw(ShouldNotSatisfied, msg)
+
+    def test_call_with_keyword_param(self):
+        def wrapper():
+            self.dinges(one=1)
+        wrapper |should| call(self.dinges).with_params(one=1)
+
+    def test_call_with_wrong_keyword_param(self):
+        def wrapper():
+            def actions():
+                self.dinges(one=2)
+            actions |should| call(self.dinges).with_params(one=1)
+        msg = ("<Dingus callable> has not been called "
+                "with params () {'one': 1}")
+        wrapper |should| throw(ShouldNotSatisfied, msg)
+
+    def test_the_lot(self):
+        def wrapper():
+            def actions():
+                self.dinges(1, one=1)
+                self.dinges(1, one=1)
+            actions |should| call(self.dinges).once.with_params(1, one=1)
+        msg = ("<Dingus callable> was called 2 times "
+                "with params (1,) {'one': 1}, expected 1")
+        wrapper |should| throw(ShouldNotSatisfied, msg)
+
